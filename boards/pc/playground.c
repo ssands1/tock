401ab04909ac81c1c19d16b8ea52ee833bbb90f2
ssands1 <ssands@princeton.edu>
2020-03-24 11:06:18 -0400

    forks new processes

--- boards/pc/playground.c
+++ boards/pc/playground.c
@@ -1,37 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
-#include "led.h"
-
 int main() {
-    setbuf(stdout, NULL); // auto-flush after each printf
-
-    char inbox[32] = { 0 };
-    int r = read(STDIN_FILENO, inbox, sizeof(inbox)/sizeof(char) - 1);
-    printf("Read %d bytes and got a message from Rust: %s", r, inbox);
-    
-    // quick and dirty sleeper
-    int i = 0;
-    while (i++ < 1000000000) 
-        if (i % 100000000 == 0)
-            printf(".\n");
-
-    printf("There are %d LED(s) on this board\n", led_count());
-    led_on(0);
-    led_off(0);
-    led_off(0);
-    led_toggle(0);
-    led_on(0);
-
+    printf("Hi, I'm a playground with PID %d\n", getpid());
     return 0;
 }
-
-/* 
-
-Notes:
- - look into protocol buffer if json is too hard
- - copy led.c into playground.c and inline the command function there
- - might be a problem using printf because tock also uses printf
- - _start function will be different in the future
- - friday Tock call if done by then.
-
-*/
--- boards/pc/src/doubler.c
+++ boards/pc/src/doubler.c
@@ -9,8 +9,8 @@
 enum {R, W};
 
 int doubler(int i) {
-    // // need to trap system calls from playgroud
-    // (void) system("./playground");
+    // need to trap system calls from playgroud
+    (void) system("./playground");
     
     int p2c[2],
         c2p[2];
@@ -22,22 +22,16 @@ int doubler(int i) {
     if (pid == ERROR) 
         return ERROR;
     if (pid == 0) { // child
+        char* outbox = "hey";
         char inbox[100];
         
         close(p2c[W]); 
         close(c2p[R]); 
-        int rVal = read(p2c[R], inbox, 100);
-        inbox[strlen(inbox)] = 'x';
-        int wVal = write(c2p[W], inbox, strlen(inbox)+1);
-        rVal = read(p2c[R], inbox, 100);
-        inbox[strlen(inbox)] = 'z';
-        wVal = write(c2p[W], inbox, strlen(inbox)+1);
-        rVal = read(p2c[R], inbox, 100);
-        inbox[strlen(inbox)] = 'Q';
-        wVal = write(c2p[W], inbox, strlen(inbox)+1);
-
+        int rVal = read(p2c[R], inbox, 100); 
         close(p2c[R]); 
+        int wVal = write(c2p[W], outbox, strlen(outbox)+1); 
         close(c2p[W]); 
+        
         if (rVal == ERROR || wVal == ERROR)
             return ERROR;
 
@@ -47,22 +41,16 @@ int doubler(int i) {
     } 
 
     // parent
-    char* outbox = "hithur";
+    char* outbox = "hi";
     char inbox[100];
     
     close(p2c[R]);
     close(c2p[W]);
-    int wVal = write(p2c[W], outbox, strlen(outbox)+1);
-    int rVal = read(c2p[R], inbox, 100);
-    inbox[strlen(inbox)] = '9';
-    wVal = write(p2c[W], inbox, strlen(inbox)+1);
-    rVal = read(c2p[R], inbox, 100);
-    inbox[strlen(inbox)] = '1';
-    wVal = write(p2c[W], inbox, strlen(inbox)+1);
-    rVal = read(c2p[R], inbox, 100);
-
-    close(c2p[R]);
-    close(p2c[W]);
+    int wVal = write(p2c[W], outbox, strlen(outbox)+1); 
+    close(p2c[W]); 
+    int rVal = read(c2p[R], inbox, 100); 
+    close(c2p[R]); 
+    
     if (rVal == ERROR || wVal == ERROR)
         return ERROR;
 
@@ -70,7 +58,3 @@ int doubler(int i) {
 
     return 2*i;
 }
-
-// int main() {
-//     doubler(0);
-// }
