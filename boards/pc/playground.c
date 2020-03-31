f78b58040b8b5568b33ba8d50ba6a1c0ec098b20
ssands1 <ssands@princeton.edu>
2020-03-31 10:38:38 -0400

    piping to and from a child process

--- boards/pc/playground.c
+++ boards/pc/playground.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
 int main() {
-    printf("Hi, I'm a playground with PID %d\n", getpid());
+    setbuf(stdout, NULL); // auto-flush after each printf
+
+    char inbox[32] = { 0 };
+    int r = read(STDIN_FILENO, inbox, sizeof(inbox)/sizeof(char) - 1);
+    printf("Read %d bytes and got a message from Rust: %s", r, inbox);
+    
+    // quick and dirty sleeper
+    int i = 0;
+    while (i++ < 1000000000) 
+        if (i % 100000000 == 0)
+            printf(".\n");
+
+    printf("All done\n");
+    
     return 0;
 }
